4da8f950568c01ade7c6d8c61f9836222ec71b6d
Ananya <ananyaananyaa124@gmail.com>
2024-11-19 19:49:21 +0530

    new

--- Web-Projects/logic.js
+++ Web-Projects/logic.js
@@ -1,2 +1,18 @@
-let mid = document.getElementById("new-task");
-
+let newtaskip = document.getElementById("new-task");
+let addbtn = document.getElementsByTagName("button")[0];
+let incomp = document.getElementById("incompleted");
+let comp = document.getElementById("completed");
+let createnew = function (taskstring) {
+    let listitem  = document.createElement("li");
+    let checkbox = document.createElement("input");
+    let label = document.createElement("label");
+    let editinput = document.createElement("input");
+    let editbtn = document.createElement("button");
+    let delbtn = document.createElement("button");
+    label.innerText = taskstring;
+    editinput.type = "text";
+    checkbox.type = "checkbox";
+    editbtn.innerText = "Edit";
+    delbtn.innerText = "delete";
+    
+}
